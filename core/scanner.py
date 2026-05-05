6f66c277887fdb0fdb2bd4f1e36eda19aff47136
isitAlo <alneami327@gmail.com>
2026-05-05 18:06:57 +0300

    Refactor device_memory update in scan function

--- core/scanner.py
+++ core/scanner.py
@@ -3,7 +3,6 @@ import subprocess
 
 def get_hostname(ip):
     try:
-        # Use nmap to find the name (Honor-90, PlayStation, etc.)
         output = subprocess.check_output(f"nmap -sL {ip}", shell=True).decode()
         if "report for" in output:
             name = output.split("report for")[1].split("(")[0].strip()
@@ -14,7 +13,6 @@ def get_hostname(ip):
     return "Unknown Device"
 
 def scan(ip_range, device_memory):
-    """Scans and updates the persistent device list."""
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     ans = scapy.srp(broadcast/arp_request, timeout=2, verbose=False)[0]
@@ -23,9 +21,5 @@ def scan(ip_range, device_memory):
         ip = element[1].psrc
         mac = element[1].hwsrc
         if ip not in device_memory or device_memory[ip]['mac'] != mac:
-            device_memory[ip] = {
-                "ip": ip, 
-                "mac": mac, 
-                "name": get_hostname(ip)
-            }
+            device_memory[ip] = {"ip": ip, "mac": mac, "name": get_hostname(ip)}
     return device_memory
