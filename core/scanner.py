41cc45c423c47323fc301929e4e9f438fffe2579
isitAlo <alneami327@gmail.com>
2026-05-05 20:46:32 +0300

    Update scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -3,11 +3,11 @@ import subprocess
 
 def get_hostname(ip):
     try:
+        # Use nmap for accurate device naming
         output = subprocess.check_output(f"nmap -sL {ip}", shell=True, stderr=subprocess.DEVNULL).decode()
         if "report for" in output:
             name = output.split("report for")[1].split("(")[0].strip()
-            if not name.replace(".", "").isnumeric():
-                return name
+            return name if not name.replace(".", "").isnumeric() else "Unknown"
     except: pass
     return "Unknown Device"
 
@@ -17,6 +17,6 @@ def scan(ip_range, device_memory):
     ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
     for element in ans:
         ip, mac = element[1].psrc, element[1].hwsrc
-        if ip not in device_memory or device_memory[ip]['mac'] != mac:
+        if ip not in device_memory:
             device_memory[ip] = {"ip": ip, "mac": mac, "name": get_hostname(ip)}
     return device_memory
