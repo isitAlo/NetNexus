ff9abb517c01bf7c9a15108ddcdde68b67e584ab
isitAlo <alneami327@gmail.com>
2026-05-05 18:21:42 +0300

    Improve subprocess call and scan timeout

--- core/scanner.py
+++ core/scanner.py
@@ -3,7 +3,7 @@ import subprocess
 
 def get_hostname(ip):
     try:
-        output = subprocess.check_output(f"nmap -sL {ip}", shell=True).decode()
+        output = subprocess.check_output(f"nmap -sL {ip}", shell=True, stderr=subprocess.DEVNULL).decode()
         if "report for" in output:
             name = output.split("report for")[1].split("(")[0].strip()
             if not name.replace(".", "").isnumeric():
@@ -14,7 +14,7 @@ def get_hostname(ip):
 def scan(ip_range, device_memory):
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    ans = scapy.srp(broadcast/arp_request, timeout=2, verbose=False)[0]
+    ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
     for element in ans:
         ip, mac = element[1].psrc, element[1].hwsrc
         if ip not in device_memory or device_memory[ip]['mac'] != mac:
