52b9be328a14a2b0f3a0ff8ffb06c161f79ac579
isitAlo <alneami327@gmail.com>
2026-05-05 14:13:21 +0300

    Refactor hostname lookup and device scanning logic
    Refactor get_hostname function to enhance name lookup using NetBIOS. Update scan function to improve device discovery process.

--- core/scanner.py
+++ core/scanner.py
@@ -2,52 +2,47 @@ import scapy.all as scapy
 import socket
 
 def get_hostname(ip):
-    """Enhanced name lookup."""
+    """
+    Tries multiple protocols to find the name.
+    """
+    # 1. Try Standard DNS
     try:
-        # Standard DNS/Reverse DNS
         return socket.gethostbyaddr(ip)[0]
-    except (socket.herror, socket.timeout):
-        try:
-            # Try to get name via NetBIOS (common for Windows/Consoles)
-            return socket.gethostbyname(ip)
-        except:
-            return "Unknown Device"
+    except:
+        pass
+
+    # 2. Try NetBIOS (The best way to find a PS5 or PC)
+    try:
+        # Sending a NetBIOS Name Service (NBNS) query
+        nbns_query = scapy.IP(dst=ip)/scapy.UDP(sport=137, dport=137)/scapy.NBNSQueryRequest(QUESTION_NAME="*")
+        ans = scapy.sr1(nbns_query, timeout=0.5, verbose=False)
+        if ans:
+            return ans.getlayer(scapy.NBNSQueryRequest).QUESTION_NAME.decode().strip()
+    except:
+        pass
+
+    return "Unknown Device"
 
 def scan(ip_range):
     """
-    Combined ARP and ICMP scan for better discovery.
+    Active discovery using ARP, ICMP, and NetBIOS.
     """
-    print(f"[*] Scanning {ip_range}...")
+    print(f"[*] Deep scanning {ip_range}...")
     
-    # 1. ARP Scan (Fastest for most devices)
+    # Send ARP broadcast
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]
+    combined_packet = broadcast/arp_request
+    
+    # We increase the timeout to 3 seconds for slower devices like the PS5
+    answered_list = scapy.srp(combined_packet, timeout=3, verbose=False)[0]
 
     devices_list = []
-    seen_ips = set()
-
     for element in answered_list:
         ip = element[1].psrc
         mac = element[1].hwsrc
         name = get_hostname(ip)
         
-        if ip not in seen_ips:
-            devices_list.append({"ip": ip, "mac": mac, "name": name})
-            seen_ips.add(ip)
-
-    # 2. ICMP (Ping) Scan for 'silent' devices that ignore ARP
-    # This wakes up devices like the PS5 or sleeping phones
-    ping_request = scapy.IP(dst=ip_range)/scapy.ICMP()
-    ans = scapy.sr(ping_request, timeout=1, verbose=False)[0]
-    
-    for sent, received in ans:
-        if received.src not in seen_ips:
-            # If we find a new IP via Ping, try to get its MAC
-            mac = "Unknown MAC"
-            name = get_hostname(received.src)
-            devices_list.append({"ip": received.src, "mac": mac, "name": name})
-            seen_ips.add(received.src)
+        devices_list.append({"ip": ip, "mac": mac, "name": name})
             
     return devices_list
