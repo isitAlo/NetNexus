21f78eeba8b97a60c5fd931ebf4309f979cc77f6
isitAlo <alneami327@gmail.com>
2026-05-07 06:36:58 +0300

    Implement concurrent hostname resolution in scanner
    Added concurrent resolution of hostnames using ThreadPoolExecutor.

--- core/scanner.py
+++ core/scanner.py
@@ -1,5 +1,6 @@
 import scapy.all as scapy
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
 def get_name(ip):
     try:
@@ -11,8 +12,18 @@ def scan(ip_range, device_memory):
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
+    
+    ips_to_resolve = []
     for element in ans:
         ip = element[1].psrc
+        mac = element[1].hwsrc
         if ip not in device_memory:
-            mac = element[1].hwsrc
-            device_memory[ip] = {"ip": ip, "mac": mac, "name": get_name(ip)}
+            device_memory[ip] = {"ip": ip, "mac": mac, "name": "Resolving..."}
+            ips_to_resolve.append(ip)
+            
+    if ips_to_resolve:
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            names = list(executor.map(get_name, ips_to_resolve))
+            
+        for ip, name in zip(ips_to_resolve, names):
+            device_memory[ip]["name"] = name
