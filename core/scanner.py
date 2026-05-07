204213639c7e100437af73935fb5d3d6fd0548f2
isitAlo <alneami327@gmail.com>
2026-05-07 06:42:17 +0300

    Refactor device name resolution in scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -1,6 +1,5 @@
 import scapy.all as scapy
 import socket
-from concurrent.futures import ThreadPoolExecutor
 
 def get_name(ip):
     try:
@@ -13,17 +12,9 @@ def scan(ip_range, device_memory):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
     
-    ips_to_resolve = []
     for element in ans:
         ip = element[1].psrc
-        mac = element[1].hwsrc
         if ip not in device_memory:
-            device_memory[ip] = {"ip": ip, "mac": mac, "name": "Resolving..."}
-            ips_to_resolve.append(ip)
-            
-    if ips_to_resolve:
-        with ThreadPoolExecutor(max_workers=50) as executor:
-            names = list(executor.map(get_name, ips_to_resolve))
-            
-        for ip, name in zip(ips_to_resolve, names):
-            device_memory[ip]["name"] = name
+            mac = element[1].hwsrc
+            name = get_name(ip)
+            device_memory[ip] = {"ip": ip, "mac": mac, "name": name}
