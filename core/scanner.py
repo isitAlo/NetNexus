5cda27209e590e0bd5080382a0922da0271da84a
isitAlo <alneami327@gmail.com>
2026-05-07 05:31:21 +0300

    Update scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -1,22 +1,12 @@
 import scapy.all as scapy
-import subprocess
 
-def get_hostname(ip):
-    try:
-        # Use nmap for accurate device naming
-        output = subprocess.check_output(f"nmap -sL {ip}", shell=True, stderr=subprocess.DEVNULL).decode()
-        if "report for" in output:
-            name = output.split("report for")[1].split("(")[0].strip()
-            return name if not name.replace(".", "").isnumeric() else "Unknown"
-    except: pass
-    return "Unknown Device"
-
-def scan(ip_range, device_memory):
+def scan(ip_range):
+    devices = {}
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
     for element in ans:
-        ip, mac = element[1].psrc, element[1].hwsrc
-        if ip not in device_memory:
-            device_memory[ip] = {"ip": ip, "mac": mac, "name": get_hostname(ip)}
-    return device_memory
+        ip = element[1].psrc
+        mac = element[1].hwsrc
+        devices[ip] = {"ip": ip, "mac": mac}
+    return devices
