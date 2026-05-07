f476a55a0b2f4df7281fd7f13045d00ed0f4bfef
isitAlo <alneami327@gmail.com>
2026-05-07 05:51:10 +0300

    Update scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -1,12 +1,18 @@
 import scapy.all as scapy
+import socket
 
-def scan(ip_range):
-    devices = {}
+def get_name(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return "Unknown"
+
+def scan(ip_range, device_memory):
     arp_request = scapy.ARP(pdst=ip_range)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     ans = scapy.srp(broadcast/arp_request, timeout=1, verbose=False)[0]
     for element in ans:
         ip = element[1].psrc
-        mac = element[1].hwsrc
-        devices[ip] = {"ip": ip, "mac": mac}
-    return devices
+        if ip not in device_memory:
+            mac = element[1].hwsrc
+            device_memory[ip] = {"ip": ip, "mac": mac, "name": get_name(ip)}
